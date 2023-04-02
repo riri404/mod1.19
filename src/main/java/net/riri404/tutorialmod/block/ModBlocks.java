b410283812329b7188d292fcdb48b1a2693c7c5c
riri404 <rama2001rifai@gmail.com>
2023-04-01 22:17:18 -0400

    Initial commit

---
+++ src/main/java/net/riri404/tutorialmod/block/ModBlocks.java
@@ -0,0 +1,15 @@
+package net.riri404.tutorialmod.block;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.riri404.tutorialmod.TutorialMod;
+
+public class ModBlocks {
+    public static final DeferredRegister<Tags.Blocks> BLOCKS=
+            DeferredRegister.create((ResourceLocation) ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
+
+
+
+}
